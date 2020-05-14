e09bfe6b7e0446e022c3cf0c48980c0b344e1fb1
Lucien Najev <luciennajev@hotmail.com>
2020-05-14 11:54:30 +0100

    add comment

--- src/components/api/axios.jsx
+++ src/components/api/axios.jsx
@@ -1,5 +1,7 @@
 import axios from 'axios';
 
+// heroku deployed api is now at
+// https://mysterious-reaches-53245.herokuapp.com/todos
 const instance = axios.create({
   baseURL: 'http://localhost:3000/',
 });
