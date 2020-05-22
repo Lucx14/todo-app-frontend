8f54dac583388364ed4333b452cba886b21d9776
Lucien Najev <luciennajev@hotmail.com>
2020-05-22 16:09:41 +0100

    adjust header links

--- src/components/Navigation/NavigationItems/NavigationItems.jsx
+++ src/components/Navigation/NavigationItems/NavigationItems.jsx
@@ -1,9 +1,12 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import { useLocation } from 'react-router-dom';
 import NavigationItem from './NavigationItem/NavigationItem';
 
 const NavigationItems = (props) => {
   const { isAuthenticated } = props;
+  const location = useLocation();
+
   return (
     <ul>
       {isAuthenticated && (
@@ -11,15 +14,15 @@ const NavigationItems = (props) => {
           Logout
         </NavigationItem>
       )}
-      {!isAuthenticated && (
-        <>
-          <NavigationItem link="/auth" exact={false}>
-            Sign in
-          </NavigationItem>
-          <NavigationItem link="/signup" exact={false}>
-            Sign up
-          </NavigationItem>
-        </>
+      {!isAuthenticated && location.pathname === '/signup' && (
+        <NavigationItem link="/auth" exact={false}>
+          Sign in
+        </NavigationItem>
+      )}
+      {!isAuthenticated && location.pathname === '/auth' && (
+        <NavigationItem link="/signup" exact={false}>
+          Sign up
+        </NavigationItem>
       )}
     </ul>
   );
